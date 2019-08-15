640d0fd14bc757edd60e22204cfcb2b3ad9ccee3
root <root@HQ-SURFACE-165.corp.indigoslate.com>
2019-08-15 10:23:02 -0700

    basic setup finished

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express'); 
 const app = express(); 
-//const routes = require('./routes'); 
+const routes = require('./routes'); 
 const bodyParser = require('body-parser'); 
 const cors = require('cors');
 
+
 app.use(bodyParser.json());
 app.use(cors());
+
+app.all('/*', routes);
 
 app.use((req, res) => res.sendStatus(404));
 
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+//const get = require('./get');
+//const send = require('./send');
+
+//router.get('/get', get);
+
+///router.put('/send', send);
+
+module.exports = router;
